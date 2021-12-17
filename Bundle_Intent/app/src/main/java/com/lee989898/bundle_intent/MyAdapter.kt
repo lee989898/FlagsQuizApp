0a652ca55b5d2eb1844949492050457b20f024d5
lee989898 <wnsrud1352@gmail.com>
2021-12-17 21:36:18 +0900

    commit

---
+++ Bundle_Intent/app/src/main/java/com/lee989898/bundle_intent/MyAdapter.kt
@@ -0,0 +1,26 @@
+package com.lee989898.bundle_intent
+
+
+import android.view.LayoutInflater
+
+import android.view.ViewGroup
+
+import androidx.recyclerview.widget.RecyclerView
+import com.lee989898.bundle_intent.databinding.ItemRecyclerviewBinding
+
+class MyViewHolder(val binding: ItemRecyclerviewBinding): RecyclerView.ViewHolder(binding.root)
+
+class MyAdapter(val datas: MutableList<String>?): RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+
+    override fun getItemCount(): Int{
+        return datas?.size ?: 0
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder
+            = MyViewHolder(ItemRecyclerviewBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val binding=(holder as MyViewHolder).binding
+        binding.itemData.text= datas!![position]
+    }
+}
